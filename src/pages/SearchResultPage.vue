5327c5902bf74207f141a7bd71f7608d6f8cda3d
itcast <hello@itcast.cn>
2024-04-02 13:05:34 +0800

    paopao伙伴匹配前端

---
+++ src/pages/SearchResultPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <van-card
+      v-for="user in userList"
+      :desc="user.profile"
+      :title="`${user.username} ${user.planetCode}`"
+      :thumb="user.avatarUrl"
+  >
+    <template #tags>
+      <van-tag plain type="danger" v-for="tag in user.tags" style="margin-right: 8px">
+        {{tag}}}
+      </van-tag>
+    </template>
+    <template #footer>
+      <van-button size="mini">联系我</van-button>
+    </template>
+  </van-card>
+  <user-card-list :user-list="userList" />
+  <van-empty v-if="!userList || userList.length <1" description="搜索结果为空" />
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref} from 'vue';
+import {useRoute} from "vue-router";
+import myAxios from "../plugins/myAxios";
+import {Toast} from "vant";
+import qs from 'qs';
+import UserCardList from "../components/UserCardList.vue";
+
+const route = useRoute();
+const {tags} = route.query;
+
+const userList = ref([]);
+
+onMounted(async () => {
+  const userListData = await myAxios.get('/user/search/tags', {
+    params: {
+      tagNameList: tags
+    },
+    paramsSerializer: params => {
+      return qs.stringify(params, {indices: false})
+    }
+  })
+      .then(function (response) {
+        // 请求成功，处理返回的数据
+        console.log(response.data);
+        //将值赋给userList才能显示
+        userList.value = response.data?.data;
+        // 返回数据
+        return response.data;
+      })
+      .catch(function (error) {
+        console.error('/user/search/tags error', error);
+        Toast.fail('请求失败');
+      })
+  console.log(userListData)
+  if (userListData) {
+    userListData.forEach((user: { tags: string; }) => {
+      if (user.tags) {
+        user.tags = JSON.parse(user.tags);
+      }
+    })
+    userList.value = userListData;
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
